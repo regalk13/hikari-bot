5397b1d6fd33b22be723c536a9672712749ce4d5
Juan Salazar <jsalazarcaviedes30@gmail.com>
2022-01-05 11:31:22 -0500

    Adding calc commands

---
+++ testbot/bot/extensions/math.py
@@ -0,0 +1,88 @@
+from lightbulb.utils.search import get
+import wolframalpha
+import lightbulb
+import hikari
+import json, asyncio, wolfram
+from hikari.colors import Color
+import urllib
+import requests
+
+plugin = lightbulb.Plugin(name="Math", description="Lows and advanced maths commands.")
+
+
+app_id = '7A6EU5-7QY3Q7RKRT'
+app = wolfram.App(app_id)
+   
+
+@plugin.command
+@lightbulb.set_help("Get the stats from wolframalpha")
+@lightbulb.option("search", "The seacrh you need")
+@lightbulb.command(name="w", aliases=("wolframalpha",), description="Seacrh something in wolframalpha")
+@lightbulb.implements(lightbulb.SlashCommand, lightbulb.PrefixCommand)
+async def command_w(ctx: lightbulb.SlashContext):
+    message = await ctx.respond("<a:Loading:893842133792997406> loading data...")
+    data = app.full(ctx.options.search)
+    data_ = data['queryresult']['pods']
+    image = None
+    title = data_[0]['subpods'][0]['img']['title']
+
+    try:
+        pods_content = data_[1]
+        sub_pods_content = pods_content['subpods']
+        image_content = sub_pods_content[0]['img']['src']
+        image = image_content
+
+    except KeyError:
+        #print(data_)
+        pods_title = data_[0]
+        #print(pods_title)
+        sub_pods_title = pods_title['subpods']
+        #print(sub_pods)
+        image_title = sub_pods_title[0]['img']['src']
+        image = image_title
+
+    embed = (hikari.Embed(
+        title=f"**{title}**",
+        color=Color(0x36393f),
+    )
+    .set_image(image)
+    .set_footer(text=f"Requestest by {ctx.member.username}#{ctx.member.discriminator}", icon=ctx.member.avatar_url)
+    )    
+
+    await message.delete()
+    await ctx.respond(embed)
+
+
+@plugin.command
+@lightbulb.set_help("Get the steps and solution of a equation")
+@lightbulb.option("equation", "Equation to solve")
+@lightbulb.command(name="calc", aliases=("ca",), description="Calculate a equation")
+@lightbulb.implements(lightbulb.SlashCommand, lightbulb.PrefixCommand)
+async def command_c(ctx: lightbulb.SlashContext):
+    equation = ctx.options.equation
+    query = urllib.parse.quote_plus(f"solve {equation}")
+    query_url = f"http://api.wolframalpha.com/v2/query?" \
+                f"appid={app_id}" \
+                f"&input={query}" \
+                f"&scanner=Solve" \
+                f"&podstate=Result__Step-by-step+solution" \
+                "&format=plaintext" \
+                f"&output=json"
+
+    r = requests.get(query_url).json()
+
+    data = r["queryresult"]["pods"][0]["subpods"]
+    result = data[0]["plaintext"]
+    steps = data[1]["plaintext"]
+
+    await ctx.respond(f"Result of {equation} is '{result}'.\n")
+    await ctx.respond(f"> Possible steps to solution:\n\n{steps}")
+
+
+
+
+def load(bot: lightbulb.BotApp) -> None:
+    bot.add_plugin(plugin)
+
+def unload(bot: lightbulb.BotApp) -> None:
+    bot.remove_plugin(plugin)
--- testbot/bot/extensions/meta.py
+++ testbot/bot/extensions/meta.py
@@ -161,6 +161,7 @@ async def command_pfp(ctx: lightbulb.SlashContext) -> None:
     )
     .set_author(name=f"{target.username}#{target.discriminator}", icon=target.avatar_url)
     .set_image(image)
+
     )
 
     await ctx.respond(embed=embed, reply=True)
